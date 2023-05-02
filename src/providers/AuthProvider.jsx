50acbeec4547251bae0cdc5ace3f446743df32e3
abdullahalarif1 <abdullahrabzil6@gmail.com>
2023-05-02 20:17:14 +0600

    auth provider and auth update done

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,16 +1,29 @@
 import React, { createContext, useContext } from "react";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import app from "../Firebase/Firebase.config";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
-
-
 const AuthProvider = ({ children }) => {
   const user = null;
+
+  const updateProfileFunction = () => {
+    return updateProfile(auth)
+  }
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
   const authInfo = {
     user,
+    createUser,
+    signIn,
+    updateProfileFunction
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
