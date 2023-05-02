fa80a5f134486776179ace2ae54d8146688136fc
abdullahalarif1 <abdullahrabzil6@gmail.com>
2023-05-02 17:08:48 +0600

    login and register design done

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Main from './Layouts/Main.jsx'
-import Home from './Pages/Home/Home.jsx'
-import Chef from './Pages/Chef/Chef.jsx'
-import router from './Routes/Router.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "./Layouts/Main.jsx";
+import Home from "./Pages/Home/Home.jsx";
+import Chef from "./Pages/Chef/Chef.jsx";
+import router from "./Routes/Router.jsx";
+import AuthProvider from "./providers/AuthProvider.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import React, { createContext, useContext } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../Firebase/Firebase.config";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const user = {  };
+  const authInfo = {
+    user,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
